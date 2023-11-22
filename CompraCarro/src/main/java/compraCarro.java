e20a375ddb76e05aa77fd469acb4e50846523fa1
MatheusSena05 <Teteusena@gmail.com>
2023-11-22 14:02:47 -0300

    inclusão do projeto

--- CompraCarro/src/main/java/compraCarro.java
+++ CompraCarro/src/main/java/compraCarro.java
@@ -4,13 +4,13 @@ public class compraCarro {
 	public static void main(String[] args) {
 		
 		
-		int valorDoCarro = 50000;
+		int preco = 500000;
 		int saldoDoCliente = 20000;
 		boolean pix = false;
 		boolean crédito = true;
 		boolean boleto = false;
-		string modelo ="Jeep Renegade 2022";
-		
+		String modelo ="Jeep Renegade 2022";
+		   
 		if(crédito) {
 			
 			System.out.println("Parabens");
